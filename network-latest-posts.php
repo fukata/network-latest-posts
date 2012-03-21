d8856a5cd7ab983f687dad0964f78fb70c24bb5d
iluminatus <iluminatus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-21 21:39:00 +0000

    Version 1.2.1: Bug fixed when calling the function inside a template's page
    git-svn-id: https://plugins.svn.wordpress.org/network-latest-posts/tags/1.2@521945 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- network-latest-posts.php
+++ network-latest-posts.php
@@ -3,7 +3,7 @@
 Plugin Name: Network Latest Posts
 Plugin URI: http://en.8elite.com/2012/02/27/network-latest-posts-wordpress-3-plugin/
 Description: This plugin allows you to list the latest posts from the blogs in your network and display them in your site using shortcodes or as a widget. Based in the WPMU Recent Posts Widget by Angelo (http://bitfreedom.com/)
-Version: 1.1
+Version: 1.2
 Author: L'Elite
 Author URI: https://laelite.info/
 */
@@ -30,10 +30,13 @@ $titleOnly: true (only title of post is displayed) OR false (title of post and n
 $begin_wrap: customise the start html code to adapt to different themes
 $end_wrap: customise the end html code to adapt to different themes
 $blog_id: allows you to retrieve the data for a specific blog
+$thumbnail: allows you to display the post's thumbnail (shortcode only)
+$cpt: allows you to display a custom post type (post, page, etc) (shortcode and widget)
+$ignore_blog: allows you to ignore one or various blogs using their ID numbers (shortcode and widget)
 
-Sample call: network_latest_posts(5, 30, true, '<li>', '</li>', null); >> 5 most recent entries over the past 30 days, displaying titles only
+Sample call: network_latest_posts(5, 30, true, '<li>', '</li>'); >> 5 most recent entries over the past 30 days, displaying titles only
 
-Sample Shortcode: [nlposts title='Latest Posts' number='2' days='30' titleonly=false wrapo='<div>' wrapc='</div>' blogid=null thumbnail=false]
+Sample Shortcode: [nlposts title='Latest Posts' number='2' days='30' titleonly=false wrapo='<div>' wrapc='</div>' blogid=null thumbnail=false cpt=post ignore_blog=null]
  * title = the section's title null by default
  * number = number of posts to display by blog 10 by default
  * days = time frame to choose recent posts from (in days) 0 by default
@@ -42,11 +45,41 @@ Sample Shortcode: [nlposts title='Latest Posts' number='2' days='30' titleonly=f
  * wrapc = html closing tag to wrap the output (for styling purposes) null by default
  * blogid = the id of the blog for which you want to display the latest posts null by default
  * thumbnail = allows you to display the thumbnail (featured image) for each post it can be true or false (false by default)
+ * cpt = custom post type, it allows you to display a specific post type (post, page, etc) (post by default)
+ * ignore_blog = this parameter allows you to ignore one or a list of IDs separated by commas (null by default) 
 */
-function network_latest_posts($how_many=10, $how_long=0, $titleOnly=true, $begin_wrap="\n<ul><li>", $end_wrap="</li></ul>", $blog_id='null', $thumbnail=false) {
+/*
+ * cpt & ignore_blog parameters were proposed by John Hawkins (9seeds.com)
+ * 
+ * Thanks for the patches, I did some tweaks to your code but it's basically the same idea
+ * I also could spot some bugs I didn't fix the last time I updated the plugin
+ * and improved the Widget lists because the <ul></ul> tags where repeating, now it's finally fixed I think
+ */
+function network_latest_posts($how_many=10, $how_long=0, $titleOnly=true, $begin_wrap="\n<li>", $end_wrap="</li>", $blog_id='null', $thumbnail=false, $cpt="post", $ignore_blog='null') {
 	global $wpdb;
 	global $table_prefix;
 	$counter = 0;
+        // Custom post type
+        $cpt = htmlspecialchars($cpt);
+        // Ignore blog or blogs
+        // if the user passes one value
+        if( !preg_match("/,/",$ignore_blog) ) {
+            // Always clean this stuff ;)
+            $ignore_blog = htmlspecialchars($ignore_blog);
+            // Check if it's numeric
+            if( is_numeric($ignore_blog) ) {
+                // and put the sql
+                $ignore = " AND blog_id != $ignore_blog ";
+            }
+        // if the user passes more than one value separated by commas
+        } else {
+            // create an array
+            $ignore_arr = explode(",",$ignore_blog);
+            // and repeat the sql for each ID found
+            for($z=0;$z<count($ignore_arr);$z++){
+                $ignore .= " AND blog_id != $ignore_arr[$z]";
+            }
+        }
 	// get a list of blogs in order of most recent update. show only public and nonarchived/spam/mature/deleted
 	if ($how_long > 0) {
                 // Select by blog id
@@ -54,11 +87,11 @@ function network_latest_posts($how_many=10, $how_long=0, $titleOnly=true, $begin
                     $blog_id = htmlspecialchars($blog_id);
                     $blogs = $wpdb->get_col("SELECT blog_id FROM $wpdb->blogs WHERE
                     public = '1' AND archived = '0' AND mature = '0' AND spam = '0' AND deleted = '0'
-                    AND blog_id = $blog_id AND last_updated >= DATE_SUB(CURRENT_DATE(), INTERVAL $how_long DAY)
+                    AND blog_id = $blog_id $ignore AND last_updated >= DATE_SUB(CURRENT_DATE(), INTERVAL $how_long DAY)
                     ORDER BY last_updated DESC");
                 } else {
                     $blogs = $wpdb->get_col("SELECT blog_id FROM $wpdb->blogs WHERE
-                    public = '1' AND archived = '0' AND mature = '0' AND spam = '0' AND deleted = '0'
+                    public = '1' $ignore AND archived = '0' AND mature = '0' AND spam = '0' AND deleted = '0'
                     AND last_updated >= DATE_SUB(CURRENT_DATE(), INTERVAL $how_long DAY)
                     ORDER BY last_updated DESC");                    
                 }
@@ -67,11 +100,11 @@ function network_latest_posts($how_many=10, $how_long=0, $titleOnly=true, $begin
                     $blog_id = htmlspecialchars($blog_id);
                     $blogs = $wpdb->get_col("SELECT blog_id FROM $wpdb->blogs WHERE
 			public = '1' AND archived = '0' AND mature = '0' AND spam = '0' AND deleted = '0' AND blog_id = $blog_id
-			ORDER BY last_updated DESC");
+			$ignore ORDER BY last_updated DESC");
                 } else {
                     $blogs = $wpdb->get_col("SELECT blog_id FROM $wpdb->blogs WHERE
 			public = '1' AND archived = '0' AND mature = '0' AND spam = '0' AND deleted = '0'
-			ORDER BY last_updated DESC");                   
+			$ignore ORDER BY last_updated DESC");                   
                 }
 	}
 
@@ -94,14 +127,14 @@ function network_latest_posts($how_many=10, $how_long=0, $titleOnly=true, $begin
 				$thispost = $wpdb->get_results("SELECT ID, post_title, post_excerpt
 					FROM $blogPostsTable WHERE post_status = 'publish'
 					AND ID > 1
-					AND post_type = 'post'
+					AND post_type = '$cpt'
 					AND post_date >= DATE_SUB(CURRENT_DATE(), INTERVAL $how_long DAY)
 					ORDER BY id DESC LIMIT 0,$how_many");
 			} else {
 				$thispost = $wpdb->get_results("SELECT ID, post_title, post_excerpt
 					FROM $blogPostsTable WHERE post_status = 'publish'
 					AND ID > 1
-					AND post_type = 'post'
+					AND post_type = '$cpt'
 					ORDER BY id DESC LIMIT 0,$how_many");
 			}
 			// if it is found put it to the output
@@ -114,12 +147,11 @@ function network_latest_posts($how_many=10, $how_long=0, $titleOnly=true, $begin
                                     // If we want to show the excerpt, we do this
                                     if ($titleOnly == false || $titleOnly == 'false') {
                                         // Widget list
-                                        if( preg_match("/\bul\b/",$begin_wrap) ) { 
+                                        if( preg_match("/\bli\b/",$begin_wrap) ) { 
                                             echo $begin_wrap.'<div class="network-posts blog-'.$blog.'"><a href="'
                                             .$thispermalink.'">'.$thispost[$i]->post_title.'</a><span class="network-posts-source">'.__('published in','trans-nlp').' <a href="'
                                             .$options[0]->option_value.'">'
                                             .$options[1]->option_value.'</a></span><p class="network-posts-excerpt">'.$thispost[$i]->post_excerpt.'</p></div>'.$end_wrap;
-                                            $counter++;
                                         // Shortcode
                                         } else {
                                             // Display thumbnail
@@ -129,33 +161,31 @@ function network_latest_posts($how_many=10, $how_long=0, $titleOnly=true, $begin
                                                 .$options[0]->option_value.'">'
                                                 .$options[1]->option_value.'</a></span><a href="'
                                                 .$thispermalink.'">'.the_post_thumbnail_by_blog($blog,$thispost[$i]->ID).'</a> <p class="network-posts-excerpt">'.$thispost[$i]->post_excerpt.'</p></div>'.$end_wrap;
-                                                $counter++;
                                             // Without thumbnail
                                             } else {
                                                 echo $begin_wrap.'<div class="network-posts blog-'.$blog.'"><h1 class="network-posts-title"><a href="'
                                                 .$thispermalink.'">'.$thispost[$i]->post_title.'</a></h1><span class="network-posts-source">'.__('published in','trans-nlp').' <a href="'
                                                 .$options[0]->option_value.'">'
                                                 .$options[1]->option_value.'</a></span><p class="network-posts-excerpt">'.$thispost[$i]->post_excerpt.'</p></div>'.$end_wrap;
-                                                $counter++;
                                             }
                                         }
                                     // Otherwise we just show the titles (useful when used as a widget)
                                     } else {
                                         // Widget list
-                                        if( preg_match("/\bul\b/",$begin_wrap) ) { 
+                                        if( preg_match("/\bli\b/",$begin_wrap) ) { 
                                             echo $begin_wrap.'<div class="network-posts blog'.$blog.'"><a href="'.$thispermalink
                                             .'">'.$thispost[$i]->post_title.'</a></div>'.$end_wrap;
-                                            $counter++;
                                         // Shortcode
                                         } else {
                                             echo $begin_wrap.'<div class="network-posts blog'.$blog.'"><h1 class="network-posts-title"><a href="'.$thispermalink
                                             .'">'.$thispost[$i]->post_title.'</a></h1></div>'.$end_wrap;
-                                            $counter++;
                                         }
                                     }
                                 }
+                                // Count only when all posts has been displayed
+                                $counter++;
 			}
-			// don't go over the limit
+			// don't go over the limit of blogs
 			if($counter >= $nblogs) {
 				break; 
 			}
@@ -174,7 +204,9 @@ function network_latest_posts_control() {
 			'number' => '10',
 			'days' => '-1',
                         'titleonly' => true,
-                        'blogid' => 'null'
+                        'blogid' => 'null',
+                        'cpt' => 'post',
+                        'ignore_blog' => 'null'
 		);
 	}
         // Save changes
@@ -184,6 +216,8 @@ function network_latest_posts_control() {
 		$options['days'] = intval($_POST['network_latest_posts_days']);
                 $options['titleonly'] = htmlspecialchars($_POST['network_latest_posts_titleonly']);
                 $options['blogid'] = htmlspecialchars($_POST['network_latest_posts_blogid']);
+                $options['cpt'] = htmlspecialchars($_POST['network_latest_posts_custompost']);
+                $options['ignore_blog'] = htmlspecialchars($_POST['network_latest_posts_ignoreblog']);
                 // Update hook
 		update_option("network_latest_posts_widget", $options);
 	}
@@ -207,17 +241,21 @@ function network_latest_posts_control() {
             <option value="null" <?php if($options['blogid'] == 'null'){ echo "selected='selected'"; } ?>><?php echo __('Display All','trans-nlp'); ?></option>
             <?php
                 global $wpdb;
-                $bids = $wpdb->get_col("SELECT blog_id FROM $wpdb->blogs WHERE
+                $bids = $wpdb->get_results("SELECT blog_id, domain FROM $wpdb->blogs WHERE
 			public = '1' AND archived = '0' AND mature = '0' AND spam = '0' AND deleted = '0'
 			ORDER BY last_updated DESC");
                 
                 foreach ($bids as $tbid) {
             ?>
-            <option value="<?php echo $tbid; ?>" <?php if($tbid == $options['blogid']){ echo "selected='selected'"; } ?>><?php echo $tbid; ?></option>
+            <option value="<?php echo $tbid->blog_id; ?>" <?php if($tbid->blog_id == $options['blogid']){ echo "selected='selected'"; } ?>><?php echo $tbid->domain." (ID ".$tbid->blog_id.")"; ?></option>
             <?php
                 }
             ?>
         </select>
+        <br /><label for="network_latest_posts_custompost"><?php echo __('Custom Post Type','trans-nlp'); ?>: </label>
+        <br /><input type="text" id="network_latest_posts_custompost" name="network_latest_posts_custompost" value="<?php echo $options['cpt'];?>" />
+        <br /><label for="network_latest_posts_ignoreblog"><?php echo __('Blog(s) ID(s) to Ignore Separate by Commas','trans-nlp'); ?>: </label>
+        <br /><input type="text" id="network_latest_posts_ignoreblog" name="network_latest_posts_ignoreblog" value="<?php echo $options['ignore_blog'];?>" />
 	<input type="hidden" id="network_latest_posts_submit" name="network_latest_posts_submit" value="1" />
 	</p>
 
@@ -237,14 +275,16 @@ function network_latest_posts_widget($args) {
 			'number' => '10',
 			'days' => '-1',
                         'titleonly' => true,
-                        'blogid' => 'null'
+                        'blogid' => 'null',
+                        'cpt' => 'post',
+                        'ignore_blog' => 'null'
 		);
 	}
         // Display the widget
 	echo $before_widget;
-	echo "$before_title $options[title] $after_title";
-	network_latest_posts($options['number'],$options['days'],$options['titleonly'],"\n<ul><li>","</li></ul>",$options['blogid']);
-	echo $after_widget;
+	echo "$before_title $options[title] $after_title <ul>";
+	network_latest_posts($options['number'],$options['days'],$options['titleonly'],"\n<li>","</li>",$options['blogid'],null,$options['cpt'],$options['ignore_blog']);
+	echo "</ul>".$after_widget;
 }
 
 // Init function
@@ -270,14 +310,16 @@ function network_latest_posts_shortcode($atts){
         'wrapo' => null,
         'wrapc' => null,
         'blogid' => 'null',
-        'thumbnail' => false
+        'thumbnail' => false,
+        'cpt' => 'post',
+        'ignore_blog' => 'null'
     ), $atts));
     // Avoid direct output to control the display position
     ob_start();
     // Check if we have set a title
     if( !empty( $title ) ) { echo "<div class='network-latest-posts-sectitle'><h1>".$title."</h1></div>"; }
     // Get the posts
-    network_latest_posts($number,$days,$titleonly,$wrapo,$wrapc,$blogid,$thumbnail);
+    network_latest_posts($number,$days,$titleonly,$wrapo,$wrapc,$blogid,$thumbnail,$cpt,$ignore_blog);
     $output_string=ob_get_contents();;
     ob_end_clean();
     // Put the content where we want
